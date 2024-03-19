79e15a085a06030771e401f094d409996776b14a
G. Y. <goktug7913@gmail.com>
2024-03-19 21:25:56 +0300

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "Init.h" //Initialization mostly done here
 #include "components/ComponentBase.hpp"
 
+/*
+  The components are stored in a vector of pointers to ComponentBase.
+  This allows for easy initialization and updating of all components.
+  Also, the components can be added and removed at runtime.
+  However, the memory layout is a bit more complex and we should
+  consider the overhead of using pointers.
+*/
 std::vector<ComponentBase*> components;
 
 void init()
@@ -24,12 +31,17 @@ void update()
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void setup()
 {
+  init();
   coldstart();
   initEscDrive(); // move into coldstart()
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void loop()
 {
+  /*
+    TODO: This function needs a total rework according to the new architecture.
+  */
+
   // Program should recover from here in a second or two during flight or the quadcopter will probably crash
   // You should get back into the flight loop as fast as possible if fallback happens
 
