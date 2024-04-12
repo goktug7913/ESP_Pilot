cd2a66177b7f6b54f6d18f044beb3a377f8e6eed
Göktuğ Yüksel <goktug7913@gmail.com>
2024-04-12 03:30:58 +0300

    cleanup

--- src/drivers/receiver/pwm.cpp
+++ src/drivers/receiver/pwm.cpp
@@ -6,14 +6,8 @@
 static const char* TAG = "PWM Receiver Driver";
 
 constexpr std::array<uint8_t, 6> RECEIVER_GPIO = {35, 34, 39, 36, 27, 14};
-constexpr int PWM_CENTER = 1500; // Center of receiver PWM pulse
-constexpr int RX_DEADZONE = 2;   // Deadzone for receiver PWM pulse
-
-constexpr int RMT_TICK_PER_US = 1;                                                 // determines how many clock cycles one "tick" is, [1..255] source is generally 80MHz APB clk
-constexpr int RMT_RX_MAX_US = 3500;                                                // time before receiver goes idle (longer pulses will be ignored)
 constexpr int PWM_CHANNELS_NUM = 6;                                                // Receiver PWM Channel Number
 constexpr std::array<uint8_t, PWM_CHANNELS_NUM> PWM_CHANNELS = {1, 2, 3, 4, 5, 6}; // Change the channels according to the number of channels
-constexpr int RMT_RX_CLK_DIV = (80000000 / RMT_TICK_PER_US / 1000000); // Divide 80Mhz APB clk by 1Mhz to get the clock for the receiver
 
 PWMReceiver::PWMReceiver() {
     type = RxType::PWM;
@@ -43,7 +37,7 @@ void PWMReceiver::init() {
             .signal_range_min_ns = 1000,
             .signal_range_max_ns = 2000,
         };
-        
+
         auto res2 = rmt_enable(channels[i]);
         ESP_ERROR_CHECK(res2);
 
